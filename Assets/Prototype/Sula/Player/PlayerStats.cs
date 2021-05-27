3ee40520ba4f0303572e3acbd7e8a860ce6fb6cb
Suletta <60387103+Suletta@users.noreply.github.com>
2021-05-27 17:27:20 +0200

    EnergyBar
    aggiunto TMP e sprite editor

--- Assets/Prototype/Sula/Player/PlayerStats.cs
+++ Assets/Prototype/Sula/Player/PlayerStats.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    //REF
+    //UIMANAGER SENNò NON VA LA UI :)
+
     //Energy
     //[SerializeField]
     public int maxEnergy = 10;
@@ -21,27 +24,30 @@ public class PlayerStats : MonoBehaviour
     {
         currentEnergy = maxEnergy;
         isFull = true;
+        //UI
+        //UpdateEnergySlider(NormalizeEnergy(maxEnergy, currentEnergy));
+
     }
 
     public void ConsumeEnergy(int energyConsumed)
     {
-
         int energyDiff = currentEnergy -= energyConsumed;
         isFull = false;
         //play sound
 
         if (energyDiff > 0)
         {
-            currentEnergy = energyDiff;            
+            currentEnergy = energyDiff;
+            //UI
+            //UpdateEnergySlider(NormalizeEnergy(maxEnergy, currentEnergy));
         }
         else
         {
             currentEnergy = 0;
             Debug.Log("Muerto");
+            //UI
+            //UpdateEnergySlider(NormalizeEnergy(maxEnergy, currentEnergy));
         }
-
-        //currentEnergy = (energyDiff > 0) ? energyDiff : 0; JUST FOR FUN
-
     }
 
     public void RefillEnergy(int energyRefill)
@@ -53,20 +59,39 @@ public class PlayerStats : MonoBehaviour
             currentEnergy = maxEnergy;
             Debug.Log("Full Energy");
             isFull = true;
+            //UI
+            //non serve aggiornare
         }
         else
         {
             currentEnergy = energySum;
             isFull = false;
+            //UI
+            //UpdateEnergySlider(NormalizeEnergy(maxEnergy, currentEnergy));
         }
+    }
 
+
+    float NormalizeEnergy(int maxEnergy, int currentEnergy) //mi serve per dare un valore sensato allo slider DA ZERO A 1
+    {        
+        float max = (float)maxEnergy;
+        float current = (float)currentEnergy;
+        float normalizedEnergy = Mathf.InverseLerp(0, max, current);
+        return normalizedEnergy;
+    }
+
+
+    void AddSavedRobot()
+    {
+        robotsSaved++;
+        //UI
+        //mettere qui la roba del ui manager
     }
 
 
 
     private void Update()
     {
-
 
 
         //ROBINE DI PROVA

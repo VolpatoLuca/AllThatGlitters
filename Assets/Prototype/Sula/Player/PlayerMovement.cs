e9f632f3a690e9dcbb63d5161e18c6ebc5c4a37f
Barattolo <76737972+Barattolo@users.noreply.github.com>
2021-06-04 11:40:58 +0200

    Revert "Merge branch 'Chiachi' into LucaV"
    This reverts commit 82fc0d370045815e3225072e027e339a12e8f158, reversing
    changes made to cb3dc768edfd1bc56f0eef480972715379edd35e.

--- Assets/Prototype/Sula/Player/PlayerMovement.cs
+++ Assets/Prototype/Sula/Player/PlayerMovement.cs
@@ -34,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         inputs = GetComponent<PlayerInputs>();
-        animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
